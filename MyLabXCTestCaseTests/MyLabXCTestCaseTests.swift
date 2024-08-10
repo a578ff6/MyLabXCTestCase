acf2df455b7b28802431c187f9c73c13a0b4e88e
a578ff6 <a578ff6@gmail.com>
2024-08-11 02:53:39 +0800

    Setup initial test cases for MyLabXCTestCase project

--- MyLabXCTestCaseTests/MyLabXCTestCaseTests.swift
+++ MyLabXCTestCaseTests/MyLabXCTestCaseTests.swift
@@ -8,29 +8,33 @@
 import XCTest
 @testable import MyLabXCTestCase
 
+
 final class MyLabXCTestCaseTests: XCTestCase {
 
+    // 在這裡設置每個測試前的初始化代碼
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    // 在這裡編寫每個測試後的清理代碼
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        // 這是第一個測試方法（先間單點到為止）
+        XCTAssertTrue(true)                 // 這裡是一個簡單的斷言
     }
 
+    // 是用來測試程式碼性能
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
+    
 
 }
+
+
