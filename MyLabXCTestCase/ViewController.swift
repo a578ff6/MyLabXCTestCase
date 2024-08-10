1c3a3236565259c67a3ad939c9e13e5124fff5f7
a578ff6 <a578ff6@gmail.com>
2024-08-10 17:21:53 +0800

    Initial Commit

---
+++ MyLabXCTestCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLabXCTestCase
+//
+//  Created by 曹家瑋 on 2024/8/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
